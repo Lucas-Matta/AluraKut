b4b228158f4b2a1c487d8ffb30c5c8d886d58909
Lucas Matta <lucasdasilvamatta89@gmail.com>
2021-07-14 14:45:10 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,43 @@
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
 
+import { AlurakutMenu } from '../src/lib/AlurakutCommons';
+
 // Função para pegar o usuario do GitHub
-function ProfileSidebar(){
-  const githubUser = 'Lucas-Matta';
+function ProfileSidebar(props){
 
   return(
     <Box>
-        <img style={{ borderRadius: '8px' }} src={`https://github.com/${githubUser}.png`} />
+        <img style={{ borderRadius: '8px' }} src={`https://github.com/${props.githubUser}.png`} />
     </Box>
   )
 }
 
 export default function Home() {
+  const githubUser = 'Lucas-Matta';
+  const pessoasFavoritas = ['juunegreiros', 'omariosouto', 'peas', 'rafaballerini', 'marcobrunodev']
 
   return(
-    <MainGrid>
-      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
-        <ProfileSidebar />
-      </div>
-      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }} >
-          <Box>
-              Bem Vindo
-          </Box>
-      </div>
-      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
-          <Box>
-              Pessoas da Comunidade
-          </Box>
-          <Box>
-              Comunidades
-          </Box>
-      </div>
-    </MainGrid>
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }} >
+            <Box>
+                Bem Vindo
+            </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+            <Box>
+                Pessoas da Comunidade
+            </Box>
+            <Box>
+                Comunidades
+            </Box>
+        </div>
+      </MainGrid>
+    </>
   )
 }

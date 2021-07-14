00d6b9c1c743dca9386fae65fd29d798da389f0a
Lucas Matta <lucasdasilvamatta89@gmail.com>
2021-07-13 21:11:51 -0300

    update

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -1,10 +1,34 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const Box = styled.div`
+    background: #FFF;
+    border-radius: 8px;
+
+`;
+
+const MainGrid = styled.main`
+    display: grid;
+    grid-gap: 10px;
+    padding: 16px;
+
+    @media(min-width: 860px){
+      grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+      grid-template-columns: 160px 618px 312px;
+    }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+    <MainGrid>
+      <Box style={{ gridArea: 'profileArea' }}>
+          imagem
+      </Box>
+      <Box style={{ gridArea: 'welcomeArea' }}>
+          Bem Vindo
+      </Box>
+      <Box style={{ gridArea: 'profileRelationsArea' }}>
+          Comunidades
+      </Box>
+    </MainGrid>
+  )
 }

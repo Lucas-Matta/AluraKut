36e06020e823cfcc8a82b7ca8b432e0b0597852f
Lucas Matta <lucasdasilvamatta89@gmail.com>
2021-07-15 17:24:47 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,17 @@ function ProfileSidebar(props){
   )
 }
 
+// Função para buscar os seguidores do GitHub
+function ProfileRelationsBox(props){
+  return(
+    <ProfileRelationsBoxWrapper>
+        <h2 className="smallTitle">
+          {props.title} ({props.items.length})
+        </h2>
+    </ProfileRelationsBoxWrapper>
+  )
+}
+
 export default function Home() {
   const githubUser = 'Lucas-Matta';
   const pessoasFavoritas = ['juunegreiros', 'omariosouto', 'peas', 'rafaballerini', 'marcobrunodev'];
@@ -35,6 +46,22 @@ export default function Home() {
         title: 'Eu odeio acordar cedo',
         image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
   }]);
+  const [seguidores, setSeguidores] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/Lucas-Matta/followers')
+  
+    // Vai transformar a resposta em JSON
+    .then((respostaDoServidor) => {
+      return respostaDoServidor.json();
+    })
+    // Vai converter para Objeto JavaScript
+    .then((respostaCompleta) => {
+      setSeguidores(respostaCompleta);
+    })
+
+
+  }, [])
 
   return(
     <>
@@ -53,32 +80,38 @@ export default function Home() {
             </Box>
             <Box>
                 <h2 className="subTitle">O que voce deseja fazer?</h2>
-                <form onSubmit={ function handleSubmit(e){
-                      const dadosDoForm = new FormData(e.target);
-
-                      const comunidade = {
-                        id: new Date().toISOString(),
-                        title: dadosDoForm.get('title'),
-                        image: dadosDoForm.get('image'),
-                      }
-
+                <form onSubmit={(e) => {
                       e.preventDefault();
-                      const comunidadesAtualizadas = [...comunidades, comunidade];
-                      setComunidades(comunidadesAtualizadas);
+
+                      if(comunidades.length >= 6 ){
+                        alert('Ops! Voce só pode criar 6 comunidades')
+                        return;
+                      } else {
+                        const dadosDoForm = new FormData(e.target);
+
+                        const comunidade = {
+                          id: new Date().toISOString(),
+                          title: dadosDoForm.get('title'),
+                          image: dadosDoForm.get('image'),
+                        }
+
+                        const comunidadesAtualizadas = [...comunidades, comunidade];
+                        setComunidades(comunidadesAtualizadas);
+                      }
                 }}>
                     <div>
-                        <input placeholder="Qual vai ser o nome da sua comunidade?"
-                                name="title"
-                                aria-label="Qual vai ser o nome da sua comunidade?"
-                                type="text"
+                        <input placeholder="Qual o nome da sua comunidade?"
+                               aria-label="Qual o nome da sua comunidade?"
+                               name="title"
+                               type="text"
                           />
                     </div>
 
                     <div>
                         <input placeholder="Coloque uma URL para usarmos como capa"
-                                name="image"
-                                aria-label="Coloque uma URL para usarmos como capa"
-                                type="text"
+                               aria-label="Coloque uma URL para usarmos como capa"
+                               name="image"
+                              type="text"
                           />
                     </div>
 
@@ -89,9 +122,12 @@ export default function Home() {
             </Box>
         </div>
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+
+            <ProfileRelationsBox items={seguidores} title="Seguidores" />
+
             <ProfileRelationsBoxWrapper>
                 <h2 className="smallTitle">
-                  Comunidades {(comunidades.length)}
+                  Comunidades ({comunidades.length})
                 </h2>
             
                 <ul>
@@ -109,19 +145,19 @@ export default function Home() {
             </ProfileRelationsBoxWrapper>
             <ProfileRelationsBoxWrapper>
                 <h2 className="smallTitle">
-                  Pessoas da Comunidade {(pessoasFavoritas.length)}
+                  Pessoas da Comunidade ({pessoasFavoritas.length})
                 </h2>
-              
+
                 <ul>
                     {pessoasFavoritas.map((githubUser) => {
-                      return(
-                        <li key={githubUser}>
-                            <a href={`/users/${githubUser}`}>
-                                <img src={`https://github.com/${githubUser}.png`} />
-                                <span>{githubUser}</span>
-                            </a>
-                        </li>
-                      )
+                        return(
+                          <li key={githubUser}>
+                              <a href={`/users/${githubUser}`}>
+                                  <img src={`https://github.com/${githubUser}.png`} />
+                                  <span>{githubUser}</span>
+                              </a>
+                          </li>
+                        )
                     })}
                 </ul>
             </ProfileRelationsBoxWrapper>

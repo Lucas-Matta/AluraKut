a9519b0271dbde29105f2b9ff35016f94051f168
Lucas Matta <lucasdasilvamatta89@gmail.com>
2021-07-14 21:39:55 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
 
@@ -26,7 +29,11 @@ function ProfileSidebar(props){
 
 export default function Home() {
   const githubUser = 'Lucas-Matta';
-  const pessoasFavoritas = ['juunegreiros', 'omariosouto', 'peas', 'rafaballerini', 'marcobrunodev']
+  const pessoasFavoritas = ['juunegreiros', 'omariosouto', 'peas', 'rafaballerini', 'marcobrunodev'];
+  const [comunidades, setComunidades] = useState([{
+        title: 'Eu odeio acordar cedo',
+        image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }]);
 
   return(
     <>
@@ -44,10 +51,56 @@ export default function Home() {
                 <OrkutNostalgicIconSet />
             </Box>
             <Box>
-              
+                <h2 className="subTitle">O que voce deseja fazer?</h2>
+                <form onSubmit={ function handleSubmit(e){
+                      const dadosDoForm = new FormData(e.target);
+
+                      const comunidade = {
+                        titulo: dadosDoForm.get('title'),
+                        image: dadosDoForm.get('image'),
+                      }
+
+                      e.preventDefault();
+                      const comunidadesAtualizadas = [...comunidades, comunidade];
+                      setComunidades(comunidadesAtualizadas);
+                }}>
+                    <div>
+                        <input placeholder="Qual vai ser o nome da sua comunidade?"
+                                name="title"
+                                aria-label="Qual vai ser o nome da sua comunidade?"
+                                type="text"
+                          />
+                    </div>
+
+                    <div>
+                        <input placeholder="Coloque uma URL para usarmos como capa"
+                                name="title"
+                                aria-label="Coloque uma URL para usarmos como capa"
+                                type="text"
+                          />
+                    </div>
+
+                    <button>
+                        Criar comunidade
+                    </button>
+                </form>
             </Box>
         </div>
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+            <ProfileRelationsBoxWrapper>
+                <ul>
+                    {comunidades.map((itemAtual) => {
+                      return(
+                        <li key={itemAtual} >
+                            <a href={`/users/${itemAtual.title}`} key={itemAtual.title}>
+                                <img src={itemAtual.image} />
+                                <span>{githubUser}</span>
+                            </a>
+                        </li>
+                      )
+                    })}
+                </ul>
+            </ProfileRelationsBoxWrapper>
             <ProfileRelationsBoxWrapper>
                 <h2 className="smallTitle">
                   Pessoas da Comunidade {(pessoasFavoritas.length)}
@@ -56,7 +109,7 @@ export default function Home() {
                 <ul>
                     {pessoasFavoritas.map((githubUser) => {
                       return(
-                        <li>
+                        <li key={githubUser}>
                             <a href={`/users/${githubUser}`} key={githubUser}>
                                 <img src={`https://github.com/${githubUser}.png`} />
                                 <span>{githubUser}</span>
